a3671b5e7c4ee4e7b13080c977731e4cdc4ffaf6
sharaquss <sharaquss@gmail.com>
2016-08-04 02:31:39 +0200

    new url patterns

--- thinkster_django_angular_boilerplate/urls.py
+++ thinkster_django_angular_boilerplate/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls import patterns, url, include
+from django.conf.urls import url, include
 from rest_framework_nested import routers
 from authentication.views import AccountViewSet
 from thinkster_django_angular_boilerplate.views import IndexView
@@ -6,8 +6,14 @@ from thinkster_django_angular_boilerplate.views import IndexView
 router = routers.SimpleRouter()
 router.register(r'accounts', AccountViewSet)
 
-urlpatterns = patterns(
-    '',
+# urlpatterns = patterns(
+#     '',
+#     url(r'^api/v1', include(router.urls)),
+#     url('^.*$', IndexView.as_view(), name='index'), #this url must be last, always
+# )
+
+urlpatterns = [
     url(r'^api/v1', include(router.urls)),
     url('^.*$', IndexView.as_view(), name='index'), #this url must be last, always
-)
+]
+
